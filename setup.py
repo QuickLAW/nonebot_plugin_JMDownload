606d062b921f80ce40af1c8084a8f165c82f1ed6
QuickLAW <89532126+QuickLAW@users.noreply.github.com>
2025-03-25 11:16:26 +0800

    chore: 更新setup.py文件以改进项目配置
    更新了setup.py文件，包括版本号升级至1.0.2，修改了项目描述为中文，添加了项目URL，并调整了classifiers以反映项目状态为Beta。此外，优化了包管理和依赖项的格式。

--- setup.py
+++ setup.py
@@ -1,49 +1,40 @@
 from setuptools import setup, find_packages
+import os
 
-VERSION = '1.0.0'
-
-with open('README.md', 'r', encoding='utf-8') as f:
+with open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
 
 setup(
-    name='nonebot-plugin-jmdownload',
-    version=VERSION,
-    description='A NoneBot2 plugin for downloading JM comics and converting them to PDF format',
+    name="nonebot-plugin-jmdownload",
+    version="1.0.2",
+    description="基于NoneBot2的JM漫画下载插件",
     long_description=long_description,
-    long_description_content_type='text/markdown',
-    author='QuickLAW',
-    author_email='yewillwork@outlook.com',
-    package_dir={'': 'src'},
-    license="BSD 3-Clause License",
-    packages=find_packages('src'),
-    package_data={
-        'nonebot_plugin_jmdownload': ['config.yml']
-    },
+    long_description_content_type="text/markdown",
+    author="QuickLAW",
+    author_email="yewillwork@outlook.com",
+    url="https://github.com/QuickLAW/nonebot_plugin_JMDownload",
+    packages=find_packages(where="src"),
+    package_dir={"":"src"},
     install_requires=[
-        'nonebot2>=2.0.0rc1',
-        'nonebot-adapter-onebot>=2.0.0',
-        'jmcomic',
-        'PyYAML',
-        'Pillow',
-        'reportlab'
+        "nonebot2>=2.0.0rc1",
+        "nonebot-adapter-onebot>=2.0.0",
+        "jmcomic",
+        "PyYAML",
+        "Pillow",
+        "reportlab"
     ],
-    python_requires='>=3.8',
+    license="BSD 3-Clause License",
+    platforms=["all"],
     classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD 3-Clause License',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10',
-        'Framework :: NoneBot',
-        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
-        'Topic :: Software Development :: Libraries :: Python Modules'
-    ],
-    keywords=['nonebot2', 'plugin', 'comic', 'download', 'pdf'],
-    project_urls={
-        'Homepage': 'https://github.com/QuickLAW/nonebot_plugin_JMDownload',
-        'Repository': 'https://github.com/QuickLAW/nonebot_plugin_JMDownload.git',
-        'Bug Reports': 'https://github.com/QuickLAW/nonebot_plugin_JMDownload/issues'
-    },
+        "Development Status :: 4 - Beta",
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Software Development :: Libraries :: Python Modules"
+    ]
 )
