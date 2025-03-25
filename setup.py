1e38122e32521312d4c7ae1f16f812c561a70e07
QuickLAW <89532126+QuickLAW@users.noreply.github.com>
2025-03-25 11:35:07 +0800

    build: 更新插件版本至1.0.4

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 setup(
     name="nonebot-plugin-jmdownload",
-    version="1.0.3",
+    version="1.0.4",
     description="基于NoneBot2的JM漫画下载插件",
     long_description=long_description,
     long_description_content_type="text/markdown",
