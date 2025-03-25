c7ce2cb63dadc566445da2ac4b980b88872cdc14
QuickLAW <89532126+QuickLAW@users.noreply.github.com>
2025-03-25 13:17:21 +0800

    refactor: 重构项目结构，将模块从src移动到nonebot_plugin_jmdownload目录
    为了更好的模块化管理和维护，将项目中的模块从src目录移动到nonebot_plugin_jmdownload目录。此次重构不涉及功能变更，仅调整文件位置以符合插件开发的最佳实践。

--- nonebot_plugin_jmdownload/__init__.py
+++ nonebot_plugin_jmdownload/__init__.py
@@ -10,7 +10,7 @@ from .utils import logger, ensure_dir
 from .handlers import jm_download  # 导入处理器
 
 
-__version__ = '1.1.7'
+__version__ = '1.2.0'
 
 __plugin_meta__ = PluginMetadata(
     name="JM漫画下载",
