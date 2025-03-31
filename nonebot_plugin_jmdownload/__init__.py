81485f3d0a5219d286fd3150e18b2969656c6368
QuickLAW <89532126+QuickLAW@users.noreply.github.com>
2025-03-31 17:17:58 +0800

    chore: 更新版本号至1.5.0b3

--- nonebot_plugin_jmdownload/__init__.py
+++ nonebot_plugin_jmdownload/__init__.py
@@ -11,7 +11,7 @@ from .utils import logger, ensure_dir
 from .handlers import jm_download  # 导入处理器
 
 
-__version__ = '1.5.0b2'
+__version__ = '1.5.0b3'
 
 __plugin_meta__ = PluginMetadata(
     name="JM漫画下载",
